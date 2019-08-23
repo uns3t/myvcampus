136f1634d771c4aa4a47d3f5ce369dc8c2ecc27d
uns3t <32424222+uns3t@users.noreply.github.com>
2019-08-23 11:25:21 +0800

    后端架构

--- vcampus_moon/src/Server/ServerThread.java
+++ vcampus_moon/src/Server/ServerThread.java
@@ -2,15 +2,9 @@ package Server;
 
 import message.*;
 import DAO.*;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
-import java.io.Serializable;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -18,70 +12,131 @@ import java.util.StringTokenizer;
 import java.lang.Exception;
 
 
-
 public class ServerThread extends Thread {
-    private Server server;
-    private int max;
-    private Connection con;
+    private Server currentServer;
+    /**
+     * 客户端Socket
+     */
+    private Socket client;
+    /**
+     * Socket对象输入流
+     */
+    private ObjectInputStream ois;
+    /**
+     * Socket对象输出流
+     */
+    private ObjectOutputStream oos;
+    /**
+     * 当前登录用户
+     */
+    public String curUser;
 
-    public void getAccessConnection(String path, String user, String pwd) {
+    public static ToAccess con;
+
+
+
+    public ServerThread(Socket s, Server st) {
+        client = s;
+        currentServer = st;
+        curUser = "";
         try {
-            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");//加载ucanaccess驱动
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
+            //建立输入输出流（次序与客户端相反）
+            ois = new ObjectInputStream(client.getInputStream());
+            oos = new ObjectOutputStream(client.getOutputStream());
+
+            System.out.println("Client connected");
+
+        } catch (IOException e) {
+            System.out.println("Cannot get IO stream");
+            e.printStackTrace();
         }
-
-        try {
-            //获取Access数据库连接(Conn)
-            this.con = DriverManager.getConnection("jdbc:ucanaccess://" + path, user, pwd);
-            System.out.println("已连接数据库");
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
-
     }
-
-
-
-    public ServerThread(Server server, int max) {
-        this.server = server;
-        this.max = max;
-        getAccessConnection("C:\\Users\\uns3t\\Desktop\\summer\\java\\idea\\myvcampus\\vcampus_moon\\database\\vcampus.accdb", "", "");
-
-    }
-
 
     public void run() {
+        con.getcon();
+        Message message=null;
         while (true) {
+            //读取消息
             try {
-                System.out.println("已启动");
-                Socket socket = server.getServerSocket().accept();
+                message = (Message) ois.readObject();
+                System.out.println(message.getType());
+            } catch (Exception e) {
+                //读不到指令，说明已登出
+                return;
+            }
 
-                ObjectInputStream ob_is = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
-                ObjectOutputStream ob_os = new ObjectOutputStream(socket.getOutputStream());
-
-                Message message = (Message) ob_is.readObject();
-                if(message.getType()=="login"){
-                    DAOlogin testlogin=new DAOlogin();
-                    LoginMessage loginMessage=(LoginMessage)message.getData();
-                    try {
-                        if(testlogin.Logincheck(con,loginMessage.getLogin_id(),loginMessage.getLogin_pwd())){
-
-                        }
-                    }catch (Exception e){
-
-                    }
-
-                }
+            //判断消息属于哪一类型，调用对应模块函数完成相应功能
+            switch (message.getType()) {
 
 
+                //登录模块
+                case "login":
+                    Login(message);
+                    break;
+
+                //学籍管理模块
+                case "student":
+                    Student();
+                    break;
+
+                //图书馆模块
+                case "book":
+                    Library();
+                    break;
+
+                //商店模块
+                case "shop":
+                    Shop();
+                    break;
+
+                //课程选择模块
+                case "course":
+                    Course();
+                    break;
+
+
+            }
+        }
+    }
+
+    public void close() {
+        if (client != null) {
+            try {
+                oos.close();
+                ois.close();
+
+
+                client.close();
+
+                currentServer.closeClientConnection(this);//在服务器线程中关闭该客户端
 
             } catch (IOException e) {
                 e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
             }
         }
+    }
+
+    //------------------------------具体功能模块----------------------------------------------------
+    //--------------------------------------------------------------------------------------------
+    public void Login(Message message){
+        LoginMessage loginMessage=(LoginMessage) message.getData();
+
+    }
+
+    public void Library(){
+
+    }
+
+    public void Student(){
+
+    }
+
+    public void Course(){
+
+    }
+
+    public void Shop(){
+
     }
 
 }
